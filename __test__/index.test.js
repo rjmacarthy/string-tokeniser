c023d8a462a8318b2edb4d32b471217487a2d8e3
rjmacarthy <richardmacarthy@protonmail.com>
2022-09-06 15:08:23 +0100

    fix tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,8 +2,8 @@ import { tokeniser } from '../index'
 import { texts, sequences, wordCounts, wordIndex, indexWord } from './fixtures'
 
 test('tokeniser tests', () => {
-  expect(tokenizer(texts).sequences).toStrictEqual(sequences)
-  expect(tokenizer(texts).wordCounts).toStrictEqual(wordCounts)
-  expect(tokenizer(texts).wordIndex).toStrictEqual(wordIndex)
-  expect(tokenizer(texts).indexWord).toStrictEqual(indexWord)
+  expect(tokeniser(texts).sequences).toStrictEqual(sequences)
+  expect(tokeniser(texts).wordCounts).toStrictEqual(wordCounts)
+  expect(tokeniser(texts).wordIndex).toStrictEqual(wordIndex)
+  expect(tokeniser(texts).indexWord).toStrictEqual(indexWord)
 })
